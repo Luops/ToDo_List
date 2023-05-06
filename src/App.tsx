ce1f488f279d1bb8c3edc4ce5783d2733ac4a74e
fabricio rios <you@example.com>
2023-05-06 12:19:04 -0300

    Iniciado o processo de armazenamento de dados dos inputs.

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,24 @@ import React from "react";
 // Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
+// Interfaces
+import { ITask } from "./interfaces/Task";
 
 function App() {
   return (
     <body className="w-100 h-screen flex flex-col justify-between font-inder">
       <Header />
-      <main className="w-100 flex flex-col min-h-[60vh] justify-center items-center py-5 gap-5 text-black text-center">
-        <div>
-          <h2>O que você vai fazer?</h2>
-          <p>Formulário</p>
+      <main className="w-100 flex flex-col min-h-[60vh] justify-start items-center py-5 gap-5 text-black text-center">
+        <div className="w-[500px]">
+          <h2 className="font-bold text-xl">O que você vai fazer?</h2>
+          <TaskForm btnText="Criar tarefa" />
         </div>
         <div>
-          <h2>Suas tarefas:</h2>
-          <p>Lista</p>
+          <h2 className="font-bold text-xl">Suas tarefas:</h2>
+          <TaskList />
         </div>
       </main>
       <Footer />
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useState, ChangeEvent, FormEvent, useEffect } from "react";
 
-type Props = {};
+// Interfaces
+import { ITask } from "../interfaces/Task";
 
-const TaskForm = (props: Props) => {
+type Props = {
+  btnText: string;
+};
+
+const TaskForm = ({ btnText }: Props) => {
+  const [id, setId] = useState<number>(0);
+  const [title, setTitle] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<number>(0);
+
+  //Função
+  const addTaskHandler = () => {};
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "title") {
+      setTitle(e.target.value); // Enviar para o State do title o valor inserido no input do título
+    } else {
+      setDifficulty(parseInt(e.target.value)); // Enviar para o State do difficulty o valor inserido no input da dificuldade em forma de número
+    }
+    console.log(title);
+    console.log(difficulty);
+  };
+
   return (
-    <div>Formulário</div>
+    <form
+      onSubmit={addTaskHandler}
+      className="w-[100%] flex flex-col justify-center gap-[1.5em]"
+    >
+      <div className="w-100 flex flex-col gap-2">
+        <label htmlFor="title" className="text-lg font-bold self-start">
+          Título:
+        </label>
+        <input
+          type="text"
+          name="title"
+          onChange={handleChange} //Quando mudar, fazer algo (Função)
+          placeholder="Título da tarefa"
+          className="border-[1px] p-1 pl-2 border-black placeholder:text-[rgba(0,0,0,0.45)]"
+        />
+      </div>
+      <div className="w-100 flex flex-col gap-2">
+        <label htmlFor="difficulty" className="text-lg font-bold self-start">
+          Dificuldade:
+        </label>
+        <input
+          type="text"
+          name="difficulty"
+          onChange={handleChange} //Quando mudar, fazer algo (Função)
+          placeholder="Dificuldade da tarefa"
+          className="border-[1px] p-1 pl-2 border-black placeholder:text-[rgba(0,0,0,0.45)]"
+        />
+      </div>
+      <input
+        type="submit"
+        value={btnText}
+        className="w-100 py-2 bg-[#61dafb] border-[1px] border-[#37a5c4] drop-shadow-[0_0_10px_#888] cursor-pointer hover:bg-black hover:text-white hover:border-black ease-in-out duration-300"
+      />
+    </form>
   );
 };
 
